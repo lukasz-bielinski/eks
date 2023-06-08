885af75d70bca73f50449e710073684d405d44c3
pulse <antynomia@gmail.com>
2023-06-08 13:14:14 +0200

    eks

--- main.tf
+++ main.tf
@@ -53,7 +53,7 @@ module "eks" {
 
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
-  cluster_endpoint_public_access = true
+  cluster_endpoint_public_access = false
   cluster_enabled_log_types   = []
   create_cloudwatch_log_group = false
 
